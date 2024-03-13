8ac476c555ebf7d1af3f9bddc0b46a9ee068103c
Gaspar <gasparhe@gmail.com>
2024-03-13 14:11:52 +0100

    estas mas calvo que una sepia
    gaspar heredia?

---
+++ proyecto-comun/src/main/java/medac/stars/proyecto/comun/ProyectoComun.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package medac.stars.proyecto.comun;
+
+/**
+ *
+ * @author gaspar
+ */
+public class ProyectoComun {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
